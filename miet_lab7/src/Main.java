121849beb7dedc04ff73ac4c9041ce87071e1f5b
Lidia Miloserdova <85784532+LidiaMil@users.noreply.github.com>
2022-05-23 15:53:48 +0300

    Update Main.java

--- miet_lab7/src/Main.java
+++ miet_lab7/src/Main.java
@@ -3,10 +3,6 @@ import javax.swing.*;
 
 import static java.lang.Thread.sleep;
 
-/**
- * @author Max Myasikov PIN-34
- * @version lab7
- */
 public class Main {
 
     public static void main(String[] args) throws Exception {
